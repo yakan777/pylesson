436684a8a1e6acdde8368666241d40ee2ef1a664
yakan777 <yume_drop@hotmail.com>
2022-07-12 14:20:17 +0900

    day5

--- day5/buss.py
+++ day5/buss.py
@@ -4,7 +4,7 @@ import folium
 
 m=folium.Map(location=[35.942957,136.198863],zoom_start=16)
 df=pd.read_csv('936.csv',encoding='shift-jis')
-buss=df[['緯度','経度']].values
+buss=df[['緯度','経度',]].values
 
 for data in buss:
     folium.Marker([data[0],data[1]]).add_to(m)
