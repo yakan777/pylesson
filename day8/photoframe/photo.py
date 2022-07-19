66a8451776d82f3c9253398a1dddc0c9b13f555b
yakan777 <yume_drop@hotmail.com>
2022-07-19 14:25:36 +0900

    day8

---
+++ day8/photoframe/photo.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+index=0
+
+def change_img():
+    global index
+    canvas.delete("PIC")
+    canvas.create_image(400,300,image=photos[index % len(photos)],tag="PIC")
+    index+=1
+    root.after(3000,change_img)
+root=tk.Tk()
+root.title("デジタルフォトフレーム")
+canvas=tk.Canvas(width=800,height=600)
+canvas.pack()
+photos=[tk.PhotoImage(file=f'cat0{i}.png') for i in range(4)]
+change_img()
+root.mainloop()
